5681601f81e54a13612f81acf3bef9706a5f3981
stiware <stiwar-quintero@hotmail.com>
2022-12-30 16:20:01 -0500

    final de pokedex v.02

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -9,16 +9,27 @@ import { PokemonService } from '../services/pokemon.service';
 })
 export class PokemonComponent implements OnInit {
 
-  name:string = "";
-  urlImage: string = "";
+  name:string = "Missingno";
   description: string = "";
+  urlImageFront: string = "";
+  urlImageShiny: string = "";
+  urlImageBack: string = "";
   constructor(private pokemonService : PokemonService) { }
   ngOnInit(): void {
   }
 
   search() {
     this.pokemonService.getPokemonPhoto(this.name).subscribe((data:any) => {
-      this.urlImage = data.sprites.other.dream_world.front_default;
+      if (data.sprites.other.dream_world.front_default !== null)
+      {
+        this.urlImageFront = data.sprites.other.dream_world.front_default;
+      }
+      else
+      {
+        this.urlImageFront= data.sprites.front_default;
+      }
+      this.urlImageBack = data.sprites.back_default;
+      this.urlImageShiny = data.sprites.front_shiny;
     this.pokemonService.getPokemonDescription(this.name).subscribe((data:any)=>{
       const resul = data.flavor_text_entries.filter((el:any) => el.language.name === 'es')
       this.description = resul[this.getRandomArbitrary(1,resul.length)].flavor_text;      
