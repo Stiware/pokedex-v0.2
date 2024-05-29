3674c873836cfc0e15de28aa8ab9f2aaefd790ff
Stiware <stiwar-quintero@hotmail.com>
2024-05-29 12:10:06 -0500

    doing a filter to pokemon types

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,54 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { PokemonModel } from 'src/app/model/pokemon.model';
 import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent {
+  Generaciones = [
+    [1, 151],
+    [152, 251],
+    [252, 386],
+    [387, 493],
+    [494, 649],
+    [650, 721],
+    [722, 809],
+    [810, 905],
+  ];
 
-  Generaciones = [[1,151],[152,251],[252,386],[387,493],[494,649],[650,721],[722,809],[810,905]]
+  constructor(private pokemonService: PokemonService) {
+
+  }
+
+  pkmStorage: PokemonModel[] = [];
+  Pokedex: PokemonModel[] = [];
   
-  constructor(private pokemonService : PokemonService) { }
+  descriptions: any[] = [['']];
 
-  Pokedex: any[] = [];
-  descriptions: any[] = [[""]];
+  gen(generacion: number) {
+    this.fillData(generacion - 1);
+  }
 
+  getDesc = (el: any[], index: number) => {
+    let res: string = el.find((el) => el[0] == index);
+    return res;
+  };
 
-gen(generacion: number){
-  this.fillData(generacion-1)
-}
-
-desc = (el: any[], index: number) =>{
-  let res:string =  el.find(el => el[0] == index)
-  return res
-}
-
-
-  fillData(generacion: number) { 
+  fillData(generacion: number) {
     this.Pokedex = [];
     this.descriptions = [];
-    for(let i = this.Generaciones[generacion][0]; i < this.Generaciones[generacion][1]; i++){
-    this.pokemonService.getpokemon(i).subscribe(
-      (data:any) =>{
+    this.pkmStorage = []
 
-       this.Pokedex.push([
-         data.id,
-         data.name,
-         data.types,
-         data.sprites.other['official-artwork'].front_default,
-        ]
-       )
-      }
-    )
+    for (let i = this.Generaciones[generacion][0]; i < this.Generaciones[generacion][1];i++)
+    {
+      this.pokemonService
+        .getPokemonDescription(i.toString())
+        .subscribe((data: any) => {
+          const resul = data.flavor_text_entries.filter(
+            (el: any) => el.language.name === 'es'
+          );
+          this.descriptions.push([data.id, resul[0].flavor_text]);
+        });
 
-    this.pokemonService.getPokemonDescription(i.toString()).subscribe((data:any) =>{
-      const resul = data.flavor_text_entries.filter((el:any) => el.language.name === 'es')
-      this.descriptions.push([data.id,resul[0].flavor_text]);    
-    })
+      this.pokemonService.getpokemon(i).subscribe((data: any) => {
+        let pkm = {
+          id: data.id,
+          name: data.name,
+          types: data.types,
+          urlImage: data.sprites.other['official-artwork'].front_default,
+          desc: this.getDesc(this.descriptions, data.id),
+        };
+        this.pkmStorage.push(pkm);
+        this.Pokedex = [...this.pkmStorage];
+      });
+    }
   }
-  console.log(this.descriptions)
+
+
+
+  filterType(tipo: string){
+    
+    
+    alert(tipo)
+
+    let aux = this.pkmStorage.filter((el) => el.types[0].type.name == tipo)
+    // let aux2 = this.Pokedex.filter((el) => el.types[2].type.name == tipo)
+    this.Pokedex = [...aux]
+    // this.Pokedex = [...aux, ...aux2]
+    
   }
+
 }
- 
---
+++ src/app/model/pokemon.model.ts
@@ -0,0 +1,7 @@
+export interface PokemonModel {
+    id: number;
+    name: string;
+    types: any;
+    urlImage: string;
+    desc: string;
+}
